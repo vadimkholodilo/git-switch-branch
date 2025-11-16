8acda4e7836a45dcc2fbef77f99d3fcfa40e95eb
vadimkholodilo <vadimkholodilo@gmail.com>
2025-11-16 10:54:19 +0300

    Add .editorconfig

--- GitSwitchBranch/GitClient/GitClient.cs
+++ GitSwitchBranch/GitClient/GitClient.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+
 using GitSwitchBranch.Models;
 
 namespace GitSwitchBranch.GitClient;
--- GitSwitchBranch/Views/BaseView.cs
+++ GitSwitchBranch/Views/BaseView.cs
@@ -4,23 +4,27 @@ namespace GitSwitchBranch.Views;
 
 public abstract class BaseView
 {
-    protected readonly int Width;
     protected readonly int Height;
+    protected readonly int Width;
 
     protected BaseView(int width, int height)
     {
         if (width <= 0)
+        {
             throw new ArgumentException("Width must be positive", nameof(width));
+        }
 
         if (height <= 0)
+        {
             throw new ArgumentException("Height must be positive", nameof(height));
+        }
 
         Width = width;
         Height = height;
     }
 
     /// <summary>
-    /// Displays a list of branches and gets a selected branch from a user
+    ///     Displays a list of branches and gets a selected branch from a user
     /// </summary>
     /// <param name="branches">List of branches</param>
     /// <returns>0-based branch index</returns>
